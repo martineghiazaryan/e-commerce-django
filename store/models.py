8f132160b965cded95630b7fa63460520dd606e9
Martin Yeghiazaryan <martineghiazaryan@gmail.com>
2022-12-11 00:27:03 +0100

    Cart added

--- store/models.py
+++ store/models.py
@@ -47,6 +47,11 @@ class OrderItem(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
